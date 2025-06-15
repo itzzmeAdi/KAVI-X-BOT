44d640e0c842763b52b7ef92c86ea164baf96989
Adithya Dimuth <110649661+itzzmeAdi@users.noreply.github.com>
2025-06-15 09:29:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~UZJjGS4Y#ukJmwZTecdFsFF4u54bmHf7203ZlzuBKEPa32UnEreE"
-global.botname = process.env.BOTNAME || "KAVI-X MD"
+global.botname = process.env.BOTNAME || "shadow - x"
 global.ownernumber = process.env.OWNERNUMBER || "94702128378"
-global.ownername = process.env.OWNERNAME || "Cyber Kavi"
+global.ownername = process.env.OWNERNAME || "shadow "
 global.websitex = process.env.WEBSITEX || "https://kavi-x-bot-login.up.railway.app/"
 global.wagc = process.env.WAGC || "https://chat.whatsapp.com/CG9f0paHJzwDImXiydfuht"
 global.botscript = process.env.BOTSCRIPT || "https://github.com/KaviDeveloperSe/KAVI-X-BOT"
 global.packname = process.env.PACKNAME || "KAVI-X MD"
-global.author = process.env.AUTHOR || "Cyber Kavi"
+global.author = process.env.AUTHOR || "shadow"
 global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
 global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
